7c71f5c8873e66fb657f5e83078706df69bc36b6
welschoff <h.welschoff@gmx.de>
2021-10-26 12:10:05 +0200

    Add params to show the name I typed in

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,14 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (_req, res) => {
-  res.send('Hello World!');
+const users = ['Marko', 'Manuel', 'Hendrik'];
+
+app.get('/api/users/:name', (request, response) => {
+  response.send(request.params.name);
+});
+
+app.get('/api/users', (_request, response) => {
+  response.send(users);
 });
 
 app.listen(port, () => {
