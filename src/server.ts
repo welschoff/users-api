5b7888abfbdd9d6667945ad3be580bb9dbe679b7
welschoff <h.welschoff@gmx.de>
2021-10-27 11:40:22 +0200

    add User on POST

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,25 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.use((request, _response, next) => {
+  console.log('Request received', request.url);
+  next();
+});
+
+app.use(express.json());
+
 const users = ['Marko', 'Manuel', 'Hendrik'];
+
+app.post('/api/users', (request, response) => {
+  const newUser = request.body;
+  if (users.includes(newUser.name)) {
+    response.status(409).send('User already exists');
+  } else users.push(newUser.name);
+  response.send(`${newUser.name} added`);
+});
 
 app.delete('/api/users/:name', (request, response) => {
   const index = users.indexOf(request.params.name);
-
   if (index > -1) {
     users.splice(index, 1);
     response.send(users);
